ed93e071e10070ffed92c7999e22e77b88d2448b
larrykoubiak <koubiak@skynet.be>
2015-08-11 09:16:21 +0200

    first commit

---
+++ ISO9660/Image.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ISO9660
+{
+    public class Image
+    {
+        private XASectorForm1[] sectors;
+        private int intNbSectors;
+        private FileStream fs;
+
+        public Image()
+        {
+            sectors = new XASectorForm1[0];
+            intNbSectors = 0;
+            fs = null;
+        }
+        public Image(string path)
+        {
+            ReadFile(path);
+        }
+
+        public XASectorForm1[] Sectors
+        {
+            get { return sectors; }
+        }
+
+        public int NbSectors
+        {
+            get { return intNbSectors; }
+        }
+
+        public void ReadFile(string path)
+        {
+            FileStream fs = new FileStream(path, FileMode.Open);
+            byte[] buffer = new byte[2352];
+            intNbSectors = (int)(fs.Length / 2352);
+            sectors = new XASectorForm1[intNbSectors];
+            for(int pos=0;pos<intNbSectors;pos++)
+            {
+                sectors[pos] = new XASectorForm1();
+                fs.Read(buffer,0, 2352);
+                sectors[pos].ReadBytes(buffer);
+            }
+            fs.Close();
+        }
+    }
+}
---
+++ ISO9660/ImageStream.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ISO9660
+{
+    class ImageStream : Stream
+    {
+        private FileStream _stream;
+
+        public ImageStream(string path, FileMode mode)
+        {
+            _stream = new FileStream(path,mode);
+        }
+        public override bool CanRead
+        {
+            get { return _stream.CanRead; }
+        }
+        public override bool CanSeek
+        {
+            get { return _stream.CanSeek; }
+        }
+        public override bool CanWrite
+        {
+            get { return _stream.CanWrite; }
+        }
+        public override void Flush()
+        {
+            _stream.Flush();
+        }
+        public override long Length
+        {
+            get { return _stream.Length; }
+        }
+        public override long Position
+        {
+            get
+            {
+                return _stream.Position ;
+            }
+            set
+            {
+                _stream.Position = value;
+            }
+        }
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            return _stream.Read(buffer,offset,count);
+        }
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            return _stream.Seek(offset, origin);
+        }
+        public override void SetLength(long value)
+        {
+            _stream.SetLength(value);
+        }
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            _stream.Write(buffer,offset,count);
+        }
+    }
+}
